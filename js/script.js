d0ad013914bd8144f8608ef48d7b771b53accf82
Pawel Wieczorek <pawel1wiecz@gmail.com>
2021-11-28 15:04:16 +0100

    Add done

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ function titleClickHandler(event){ //event- kliknięcie
       activeLink.classList.remove('active');
     }
 
-  /* [IN DONE PROGRESS]add class 'active' to the clicked link */
+  /* [DONE ]add class 'active' to the clicked link */
   console.log('clickedElement', clickedElement);
   clickedElement.classList.add('active');
 
@@ -24,14 +24,15 @@ function titleClickHandler(event){ //event- kliknięcie
         activeArticle.classList.remove('active');
     }
 
-  /* get 'href' attribute from the clicked link */
+  /*[DONE] get 'href' attribute from the clicked link */
   const articleSelector = clickedElement.getAttribute("href"); /*zwraca wartość atrybutu href */
   console.log(articleSelector);
-  /* find the correct article using the selector (value of 'href' attribute) */
+
+  /* [DONE]find the correct article using the selector (value of 'href' attribute) */
   const targetArticle = document.querySelector(articleSelector);
   console.log(targetArticle);
 
-  /*add class 'active' to the correct article*/ 
+  /* [DONE]add class 'active' to the correct article*/ 
   targetArticle.classList.add('active');
 }
 
@@ -40,3 +41,5 @@ const links = document.querySelectorAll('.titles a');
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
+
