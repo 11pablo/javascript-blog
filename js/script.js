ec0bad236e5367c9c58352549ee4d6106d28ae72
Pawel Wieczorek <pawel1wiecz@gmail.com>
2021-11-29 19:20:03 +0100

    Add empty function generateTitleLinks

--- js/script.js
+++ js/script.js
@@ -43,3 +43,26 @@ for(let link of links){
 }
 
 
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+
+  /* for each article */
+
+    /* get the article id */
+
+    /* find the title element */
+
+    /* get the title from the title element */
+
+    /* create HTML of the link */
+
+    /* insert link into titleList */
+
+}
+
+generateTitleLinks();
